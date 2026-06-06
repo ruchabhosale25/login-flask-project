185ddc99814ec0fe7a9cb283ac1086f9e231412f
Rucha Bhosale <ruchabhosale789@gmail.com>
2026-06-06 14:12:47 +0530

    updated app.py

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ app.secret_key = "mysecretkey"
 
 # Connect MySQL
 db = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME"),
-    port=int(os.getenv("DB_PORT"))
+    host=os.environ.get("DB_HOST"),
+    user=os.environ.get("DB_USER"),
+    password=os.environ.get("DB_PASSWORD"),
+    database=os.environ.get("DB_NAME"),
+    port=int(os.environ.get("DB_PORT"))
 )
 
 # LOGIN PAGE
